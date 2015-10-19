10c89e58b9af4e583a7b5262140768ad63142407
ThorMutoAsmund <thorasmund@gmail.com>
2015-10-19 22:48:31 +0200

    Added config.js

---
+++ src/config.js
@@ -0,0 +1,24 @@
+
+
+Pebble.addEventListener('showConfiguration', function(e) {
+  // Show config page
+  Pebble.openURL('https://dfilskov.github.io/quarterweek/');
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {
+  // Decode and parse config data as JSON
+  var configData = JSON.parse(decodeURIComponent(e.response));
+  console.log('Config window returned: ', JSON.stringify(configData));
+
+  // Prepare AppMessage payload
+  var dict = {
+    'KEY_CHANGE_INVERT_ON_STARTUP': configData[animSetting],
+  };
+
+  // Send settings to Pebble watchapp
+  Pebble.sendAppMessage(dict, function(){
+    console.log('Sent config data to Pebble');  
+  }, function() {
+    console.log('Failed to send config data!');
+  });
+});
